bff23fb2d0619f934e319a4d76eff31862dd714a
Peter Oun <57410426+PeterWorakarn@users.noreply.github.com>
2021-06-30 09:16:01 +0700

    Add Pixabay route

---
+++ src/routes/Pixabay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from '../components/image/Image'
+
+// consume content a bit by bit
+
+export default function Pixabay() {
+    return (
+        <div>
+            <Image />
+        </div>
+    )
+}
