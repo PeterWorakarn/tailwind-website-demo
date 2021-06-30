b6e0e8737ec7b53e13a10444288c03e9a04261c7
Peter Oun <57410426+PeterWorakarn@users.noreply.github.com>
2021-06-30 09:15:46 +0700

    implement optimization with suspense and react lazy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './routes/Home';
 import About from './routes/About';
 import Blog from './routes/Blog';
 import Contact from './routes/Contact';
 import useReducer1 from './routes/useReducer1';
 import useReducer2 from './routes/useReducer2';
+import Pixabay from './routes/Pixabay';
 import InfinityScroll from './routes/InfinityScroll';
 import Notfound from './routes/Notfound';
+
+const Header = lazy(() => import('./components/Header'))
+const Footer = lazy(() => import('./components/Footer'))
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen relative">
-        <Header />
+        <Suspense fallback={<div />}>
+          <Header />
+        </Suspense>
         <main>
-        <Switch>
+          <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/blog" component={Blog} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
-            <Route path="/useReducer1" component={useReducer1} />
-            <Route path="/useReducer2" component={useReducer2} />
-            <Route path="/InfinityScroll" component={InfinityScroll} />
+            <Route path="/usereducer1" component={useReducer1} />
+            <Route path="/usereducer2" component={useReducer2} />
+            <Route path="/infinityscroll" component={InfinityScroll} />
+            <Route path="/pixabay" component={Pixabay} />
             <Route path="*" component={Notfound} />
           </Switch>
         </main>
-        <Footer />
+        <Suspense fallback={<div />}>
+          <Footer />
+        </Suspense>
       </div>
     </Router>
   );
